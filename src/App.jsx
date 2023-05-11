3b1cdf554c19b5787bbb983a8ebf6dd8f7301502
michimalonco <vargas.mati1999@gmail.com>
2023-05-11 00:24:05 -0400

    mami

--- src/App.jsx
+++ src/App.jsx
@@ -40,10 +40,11 @@ function App() {
             src="/equidad1.png"
             width="100%"
             height="100%"
+            paddingBottom="10vh"
             sx={{}}
           />
           <a href="https://twitter.com/intent/tweet?text=No%20pagues%20tu%20cae%20enterate%20aqui%20&url=http%3A%2F%2Fequidadlegal.cl&hashtags=programación,html" target="_blank"><h1 translate="no" className='prescribelaw'>#PRESCRIBELAW</h1></a>
-                  
+          <a href="https://www.instagram.com/equidadlegal.cl/" target="_blank"><h1 translate="no" className='prescribelaw'>#NOMASCAE</h1></a>       
           <Box sx={{paddingTop:"20%",position:"fixed",display:"flex",flexDirection:"column",justifyContent:"space-between",  bottom: 0,
   right: 0}}>
             <Box
