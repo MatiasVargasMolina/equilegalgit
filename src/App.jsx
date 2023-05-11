1b48f5c86feec248b4bd0cef61d0ba318afc538b
michimalonco <vargas.mati1999@gmail.com>
2023-05-11 01:30:00 -0400

    ytfhgh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
@@ -9,7 +9,26 @@ function App() {
   function redirigirWsp() { 
     window.location.replace("https://wa.me/56971997994/?text=Hola, me podrias enviar mas informacion?");
   }
-
+  const [hashtag,setHashtag]=useState([{nombre:"#FONDOSOLIDARIO",link:"https://twitter.com/intent/tweet?text=No%20pagues%20tu%20cae%20enterate%20aqui%20&url=http%3A%2F%2Fequidadlegal.cl&hashtags=programación,html"},{nombre:"#NOMASCAE",link:"https://www.instagram.com/equidadlegal.cl"},{nombre:"#PRESCRIBELAW",link:"https://www.instagram.com/equidadlegal.cl"}]);
+  const [position,setPosition]=useState(0);
+  function cambiar(){
+    console.log(position)
+    if(position==2){
+      setPosition(0);
+    }
+    else if(position==0){
+      setPosition(1)
+    }
+    else if(position==1){
+      setPosition(2)
+    }
+  }
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setPosition((position + 1) % hashtag.length);
+    }, 3000);
+    return () => clearInterval(intervalID);
+  }, [position, hashtag.length]);
 
   return (
     <div className='conteiner'>
@@ -18,7 +37,7 @@ function App() {
         JUSTICIA AL ALCANCE DE TOD@S
         </h1>
       </div>
-      <Box sx={{ display:"flex",  justifyContent: "center",paddingTop:"8vh", /* Centrar horizontalmente */
+      <Box sx={{ display:"flex",  justifyContent: "center",paddingTop:"5vh",paddingBottom:"5vh", /* Centrar horizontalmente */
   alignItems: "center",flexDirection:"column",width:"100vh"}}>
         <Box
               width="50%"
@@ -41,12 +60,10 @@ function App() {
             src="/equidad1.png"
             width="100%"
             height="100%"
-            paddingBottom="10vh"
+            paddingBottom="20vh"
             sx={{}}
           />
-          <a href="https://twitter.com/intent/tweet?text=No%20pagues%20tu%20cae%20enterate%20aqui%20&url=http%3A%2F%2Fequidadlegal.cl&hashtags=programación,html" target="_blank"><h1 translate="no" className='prescribelawTouch'>#PRESCRIBELAW</h1></a>
-          <a href="https://www.instagram.com/equidadlegal.cl/" target="_blank"><h1 translate="no" className='prescribelaw'>#FONDOSOLIDARIO</h1></a>
-          <a href="https://www.instagram.com/equidadlegal.cl/" target="_blank"><h1 translate="no" className='prescribelaw'>#NOMASCAE</h1></a>       
+          <a href={hashtag[position].link} target="_blank"><h1 translate="no" className='prescribelawTouch'>{hashtag[position].nombre} </h1></a>    
           <Box sx={{paddingTop:"20%",position:"fixed",display:"flex",flexDirection:"column",justifyContent:"space-between",  bottom: 0,
   right: 0}}>
             <Box
